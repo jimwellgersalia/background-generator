9f32ecc13101c78c0bf830dedd4fc4e45dec4bd3
Jimwell Gersalia <jimwellgersalia@gmail.com>
2024-10-13 20:54:35 +0800

    replacing old syntax to new way of syntax based on advanced js

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+//Newer  modern way to write the code (this new syntax will be explored in Advanced JS section)
+const css = document.querySelector('h3');
+const [color1, color2] = document.querySelectorAll('.color')
+const body = document.querySelector('#gradient');
 
-
-function setGradient(){
-	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-
-	css.textContent = body.style.background + ";";
+setGradient = () => {
+	body.style.background = _linearGradient();
+	css.textContent = `${body.style.background};`;
 }
 
-color1.addEventListener("input", setGradient);
 
-color2.addEventListener("input", setGradient);
+_linearGradient = () => {
+	return `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+}
+
+color1.addEventListener('input', setGradient);
+color2.addEventListener('input', setGradient);
+
+
+
+
+// var css = document.querySelector("h3");
+// var color1 = document.querySelector(".color1");
+// var color2 = document.querySelector(".color2");
+// var body = document.getElementById("gradient");
+
+
+// function setGradient(){
+// 	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
+// 	css.textContent = body.style.background + ";";
+// }
+
+// color1.addEventListener("input", setGradient);
+
+// color2.addEventListener("input", setGradient);
 
